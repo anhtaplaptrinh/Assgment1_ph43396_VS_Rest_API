a3a5e1539242653181160a9105e533ae33445712
acertuananh <anhttph43396@fpt.edu.vn>
2024-04-08 22:58:43 +0700

    asm2

--- api.js
+++ api.js
@@ -1,54 +1,57 @@
 const express = require('express')
 const router = express.Router();
-const sanphamModel = require('./SanphamModel')
-const mongoose = require('mongoose');
+const sanphamModel = require('./model/SanphamModel')
 const upload = require('./config/common/upload')
-const path = require('path');
+const users = require('./model/userModel');
+const unidecode = require('unidecode');
 
 module.exports = router;
 
 router.get('/', (req, res) => {
     res.send('Vao api Moblie')
 })
-
-const uri = 'mongodb+srv://anhttph43396:EeQIOMkkPfdZRLRk@lab3-anhttph43396.9jz7xzv.mongodb.net/Assigment_REST_API';
-
 router.get('/list', async (req, res) => {
-    await mongoose.connect(uri)
-
     let sanpham = await sanphamModel.find();
 
     res.send(sanpham)
 })
 
-router.post('/add', async (req, res) => {
+router.post('/add', upload.single('anh'), async (req, res) => {
     try {
-        const newSanpham = new sanphamModel(req.body);
-        await newSanpham.save();
-        res.status(201).send('Dữ liệu đã được thêm thành công');
+        const data = req.body;
+        const { file } = req
+        const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${file.filename}`
+
+        const newSanpham = new sanphamModel({
+            ten: data.ten,
+            anh: imageUrl,
+            gia: data.gia,
+            soluong: data.soluong,
+        });
+
+        const result = await newSanpham.save();
+
+        if (result) {
+            res.json({
+                "status": 200,
+                "message": "Thêm thành công",
+                "data": result
+            });
+        } else {
+            res.json({
+                "status": 400,
+                "message": "Thêm không thành công",
+                "data": []
+            });
+        }
     } catch (error) {
         console.error('Lỗi khi thêm dữ liệu:', error);
         res.status(500).send('Đã xảy ra lỗi khi thêm dữ liệu');
     }
 });
- 
-router.get('/gallery/:imageName', async (req, res) => {
-    await mongoose.connect(uri)
-    try {
-        const imageName = req.params.imageName;
- 
-        res.render('gallery', { images: [imageName] });
-    } catch (error) {
-        console.error('Lỗi khi tìm ảnh:', error);
-        res.status(500).send('Đã xảy ra lỗi khi tìm ảnh');
-    }
-});
-
 
 router.delete('/delete/:id', async (req, res) => {
     try {
-        await mongoose.connect(uri);
-
         let id = req.params.id;
         let result = await sanphamModel.deleteOne({ _id: id });
 
@@ -71,22 +74,166 @@ router.delete('/delete/:id', async (req, res) => {
     }
 });
 
-router.put('/update/:id', async (req, res) => {
+router.put('/update/:id', upload.single('anh'), async (req, res) => {
     try {
-        await mongoose.connect(uri);
+        const id = req.params.id;
+        const data = req.body;
+        const { file } = req
+        const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${file.filename}`
 
+        const result = await sanphamModel.findByIdAndUpdate(id, {
+            ten: data.ten,
+            anh: imageUrl,
+            gia: data.gia,
+            soluong: data.soluong,
+        })  
+
+        if (result) {
+            res.json({
+                "status": 200,
+                "message": "Cập nhật thành công",
+                "data": result
+            });
+        } else {
+            res.json({
+                "status": 400,
+                "message": "Không tìm thấy trái cây",
+                "data": []
+            });
+        }
+    } catch (error) {
+        console.error('Lỗi khi thêm dữ liệu:', error);
+        res.status(500).send('Đã xảy ra lỗi khi thêm dữ liệu');
+    }
+});
+
+router.put('/update-no-image/:id', upload.single('anh'), async (req, res) => {
+    try {
         const id = req.params.id;
         const data = req.body;
 
-        const updateFruit = await sanphamModel.findByIdAndUpdate(id, data, { new: true });
+        const result = await sanphamModel.findByIdAndUpdate(id, {
+            ten: data.ten,
+            gia: data.gia,
+            soluong: data.soluong,
+        })  
 
-        res.json({
-            "status": 200,
-            "messenger": "Cập nhật thành công",
-            "data": updateFruit
-        });
+        if (result) {
+            res.json({
+                "status": 200,
+                "message": "Cập nhật thành công",
+                "data": result
+            });
+        } else {
+            res.json({
+                "status": 400,
+                "message": "Cap nhat that bai",
+                "data": []
+            });
+        }
     } catch (error) {
-        console.error('Lỗi khi cập nhật sản phẩm:', error);
+        console.error('Lỗi khi thêm dữ liệu:', error);
+        res.status(500).send('Đã xảy ra lỗi khi thêm dữ liệu');
+    }
+});
+
+router.post('/register', upload.single('avatar'), async (req, res) => {
+    try {
+        const data = req.body;
+        const { file } = req
+        const avatar = `${req.protocol}://${req.get("host")}/uploads/${file.filename}`
+        const newUser = users({
+            username: data.username,
+            password: data.password,
+            email: data.email,
+            name: data.name,
+            avatar: avatar,
+        })
+
+        const result = await newUser.save()
+
+        if (result) { 
+            res.json({
+                "status": 200,
+                "messenger": "Thêm thành công",
+                "data": result
+            })
+        } else {
+            res.json({
+                "status": 400,
+                "messenger": "Lỗi, thêm không thành công",
+                "data": []
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await users.findOne({ username, password })
+        if (user) {
+            res.json({
+                "status": 200,
+                "messenger": "Đăng nhâp thành công",
+                "data": user
+            })
+        } else {
+            res.json({
+                "status": 400,
+                "messenger": "Lỗi, đăng nhập không thành công",
+                "data": []
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get('/search', async (req, res) => {
+    try {
+        const tuKhoa = req.query.key; 
+       
+        const ketQuaTimKiem = await sanphamModel.find({ ten: { $regex: new RegExp(tuKhoa, "i") } });
+
+        if (ketQuaTimKiem.length > 0) {
+            res.json(ketQuaTimKiem);
+        } else {
+            res.json([]);
+        }
+    } catch (error) {
         res.status(500).send('Lỗi máy chủ nội bộ');
+    }
+});
+
+const sapxepgiamdan = (products) => {
+    return products.sort((a, b) => b.gia - a.gia);
+}
+
+router.get('/giam-dan', async (req, res) => {
+    try {
+        let sanpham = await sanphamModel.find();
+        let sortedProducts = sapxepgiamdan(sanpham);
+        res.json(sortedProducts);
+    } catch (error) {
+        console.error('Lỗi khi sắp xếp danh sách sản phẩm theo giá tăng dần:', error);
+        res.status(500).send('Đã xảy ra lỗi khi sắp xếp danh sách sản phẩm');
+    }
+});
+
+const sapxeptangdan = (products) => {
+    return products.sort((a, b) => a.gia - b.gia);
+}
+
+router.get('/tang-dan', async (req, res) => {
+    try {
+        let sanpham = await sanphamModel.find();
+        let sortedProducts = sapxeptangdan(sanpham);
+        res.json(sortedProducts);
+    } catch (error) {
+        console.error('Lỗi khi sắp xếp danh sách sản phẩm theo giá giảm dần:', error);
+        res.status(500).send('Đã xảy ra lỗi khi sắp xếp danh sách sản phẩm');
     }
 });
--- server.js
+++ server.js
@@ -1,31 +1,36 @@
 const express = require('express')
 const app = express();
 const port = 3000
-
-const sanhphamModel = require('./SanphamModel')
 const mongoose = require('mongoose');
 
+const sanphamModel = require('./model/SanphamModel')
+
 const path = require('path');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
 
 app.listen(port, () => {
     console.log(`Server đang chạy ở cổng ${port}`)
 })
 
-const uri = 'mongodb+srv://anhttph43396:EeQIOMkkPfdZRLRk@lab3-anhttph43396.9jz7xzv.mongodb.net/Assigment_REST_API';
+const uri = 'mongodb+srv://anhttph43396:anh@lab3-anhttph43396.9jz7xzv.mongodb.net/Assigment_REST_API';
+
 
 app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
-const api = require('./api')
+const api = require('./api');
 app.use('/api', api)
 
-app.get('/', async (req, res) => {
-    await mongoose.connect(uri)
+try {
+    mongoose.connect(uri)
+    console.log('Ket noi thanh cong');
+} catch (error) {
+    console.log('Loi: ', error);
+}
 
-    let sanpham = await sanhphamModel.find();
+app.get('/', async (req, res) => {
+    let sanpham = await sanphamModel.find();
 
     res.send(sanpham)
 })
